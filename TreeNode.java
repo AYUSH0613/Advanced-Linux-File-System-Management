962df452766a3c99271fe81905ef77010422eb21
AYUSH0613 <ayushparoliya2004@gmail.com>
2025-07-13 01:34:13 +0530

    Add TreeNode class for file system representation

---
+++ TreeNode.java
@@ -0,0 +1,30 @@
+package LFMS;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TreeNode {
+    String name;
+    char type; // 'd' for directory, '-' for file
+    String cdate, mdate;
+    int permission;
+    TreeNode parent, firstChild, nextSibling;
+
+    public TreeNode(String name, char type, TreeNode parent) {
+        this.name = name;
+        this.type = type;
+        this.parent = parent;
+        this.permission = 777;
+        this.cdate = this.mdate = currentTime();
+        this.firstChild = this.nextSibling = null;
+    }
+
+    private String currentTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    public String getPermissionString() {
+        return String.format("%03d", permission);
+    }
+}
